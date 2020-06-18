13d70c9d3379dabee5dd4cd9898b0b777dfb92ae
Ammar2j <ammarahmed89754@gmail.com>
2020-06-19 02:05:07 +0500

    color changing issue fixed

--- src/Room.js
+++ src/Room.js
@@ -5,7 +5,7 @@ function Room() {
 
     let [islit,setlit] = useState(true);
     return(
-    <div className = { "room"+(islit? 'lit': 'dark') }>
+    <div className = { "room "+(islit? 'lit': 'dark') }>
         the room is {islit? 'lit':'dark'}
         <br/>
         <button onClick = { ()=> setlit(!islit)}>
